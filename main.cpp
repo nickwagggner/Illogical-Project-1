df9c8bc859d319191fad1479f1a4aaa2feb4e64e
bla1232 <92119509+bla1232@users.noreply.github.com>
2021-10-08 11:45:22 -0500

    Add files via upload
    main menu

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+  	char c;
+  	int num;
+  	while(c!='5'){
+	    cout << "Enter a number to be converted\n" << "1) Convert base 10 to binary\n" << "2) Convert binary to base 10\n" << "3) Convert base 16 to binary\n" << "4) Convert binary to base 16\n" << "5) End Program" <<endl;
+	    cin>> c;
+	    if(c=='1'){
+	    	cout<< "Enter a Base10 number to be converted to Binary: ";
+	    	cin>>num;
+	    	cout<<"\nResult: "<<baseTentoTwo(num)<<endl;
+	    }else if (c=='2'){
+	    	cout << "Enter a Binary number to be converted to Base10: ";
+	    	cin>>num;
+	    	cout<<"\nResult: "<<baseTwotoTen(num)<<endl;//change function names if needed
+	    }else if (c=='3'){
+	    	cout << "Enter a Base16 number to be converted to Binary: ";
+	    	cin>>num;
+	    	cout<<"\nResult: "<<baseSixTeentoTwo(num)<<endl;
+	    }else if (c=='4'){
+	    	 cout<< "Enter a Binary number to be converted to Base16: ";
+	    	 cin>>num;
+	    	 cout<<"\nResult: "<<baseTwotoSixTeen(num)<<endl;
+	    }else{
+	    	cout << "enter correct input"; 
+	    }
+	    num=0;//clearing out num
+	}
+
+	return 0;
+}
