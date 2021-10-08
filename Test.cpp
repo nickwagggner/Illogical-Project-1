1076df4739bbbf162a5ac450d7abe5b1b36c22c7
Skyeler Knuuttila <92111111+SkyeKnuuttila@users.noreply.github.com>
2021-10-08 12:36:57 -0500

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[])
   char input;		// initialize a value for reading in inputs
   
   // initialize arrays for bases two and sixteen
-  string listTwo[] = {"0","1","10","11","100","101","110","111","1000","1001","1010","1011","1100","1101","1110","1111"};
+  string listTwo[] = {"0000","0001","0010","0011","0100","0101","0110","0111","1000","1001","1010","1011","1100","1101","1110","1111"};
   string listSixt[] = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
   
   string val;		// binary / base sixteen value to read in
